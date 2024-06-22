c33c3566ed9e744b540d6aa11ff66271c56630dd
chas <scxrfxcer@gmail.com>
2024-06-22 00:19:22 -0300

    stardew_valley1

---
+++ teste js/teste.js
@@ -0,0 +1,23 @@
+// const permissoes = "admin";
+
+// // switch(permissoes){
+
+// // case "aluno":
+
+// // console.log("negado");
+
+// // case "admin":
+
+// // console.log("aceito");
+
+// // default:
+
+// // console.log("n sei quem é vc no sistema")
+
+
+
+// // }
+
+idade = 10
+
+idade >= 18 ? console.log("maior") : console.log("menor")
